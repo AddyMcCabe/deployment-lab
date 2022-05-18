232ab23b5be96e4cafdde896b66b4ebda9b2cf5b
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-18 14:04:26 -0700

    ready to deploy

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+})
